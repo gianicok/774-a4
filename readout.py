803ef7dd9df9b2bf72c68562e42b0991bd7d3ddb
gianicok <kostagianicos@gmail.com>
2024-06-06 14:42:40 -0400

    added disc sims

--- readout.py
+++ readout.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-folder = './cah20_b/'
+folder = './cah20_d/'
 
 interest = 0
 with open(folder+'egs5job.out', 'r') as file:
@@ -39,5 +39,28 @@ ax.scatter(u, v, w, marker='o')
 ax.set_xlabel('U')
 ax.set_ylabel('V')
 ax.set_zlabel('W')
-ax.set_title('EGS5 uin, vin, win')
+ax.set_title('Source Direction: Part a')
+plt.show()
+
+x, y, z = [], [], []
+with open(folder+'positions.out', 'r') as file:
+    lines = file.readlines()
+    for i in range(len(lines)):
+        line = lines[i].strip().split()
+        x.append(float(line[1]))
+        y.append(float(line[2]))
+        z.append(float(line[3]))
+
+# plot results
+fig = plt.figure()
+ax = fig.add_subplot(projection='3d')
+#ax.set_xlim3d(-1,1)
+#ax.set_ylim3d(-1,1)
+#ax.set_zlim3d(-1,1)
+ax.scatter(x, y, z, marker='o')
+#ax.quiver(0, 0, 0, u, v, w,linewidth=1)
+ax.set_xlabel('X')
+ax.set_ylabel('Y')
+ax.set_zlabel('Z')
+ax.set_title('Source Positions: Part a')
 plt.show()
